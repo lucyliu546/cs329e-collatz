3d97b0dbdff464f0ce0968437eadd00737b0237a
Lucy Liu <lucyliu546@gmail.com>
2017-02-02 16:03:44 -0600

    Shortened cache

--- NumberGenerator.py
+++ NumberGenerator.py
@@ -4,7 +4,7 @@ def main():
 
     start = 0
     end = 1000000
-    for x in range(0, 999999, 20000):
-        i, j = (x, x + 20000)
+    for x in range(0, 999999, 5000):
+        i, j = (x, x + 5000)
         print(i, j)
 main()
